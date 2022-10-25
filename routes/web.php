293fe892ce075b27cfcc96ff1f1e61fc9641ffbc
VyPham23 <vy.pham200210@vnuk.edu.vn>
2022-10-25 20:06:35 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('index',[PageController::class, 'getIndex']);
+Route::get('loai-san-pham',[PageController::class, 'getLoaiSp']);
+Route::get('chi-tiet-san-pham',[PageController::class, 'getChitiet']);
+Route::get('lien-he', [PageController::class, 'getLienhe']);
+Route::get('gioi-thieu',[PageController::class, 'getAbout']);
