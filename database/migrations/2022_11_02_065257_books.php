b0eeb6288fe56a66505651a94bd80e69f79a3c11
VyPham23 <vy.pham200210@vnuk.edu.vn>
2022-11-02 14:05:19 +0700

    commit to main

---
+++ database/migrations/2022_11_02_065257_books.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @var string
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('author');
+            $table->integer('category');
+            $table->integer('price');
+            $table->string('image');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
